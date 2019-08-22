05d46663161449ef20b3139cdb8e8b1268a4cadd
akirahin <niharikakulkarni@hotmail.com>
2019-08-22 19:17:24 +0530

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Recipes from './components/Recipes';
+import List from './components/List';
 
-function App() {
-  return (
-    <div>
-        Hello
-    </div>
-  );
+function AppRouter() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Recipes</Link>
+                        </li>
+                        <li>
+                            <Link to="/list/">Shopping List</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Route path="/" exact component={Recipes} />
+                <Route path="/list/" component={List} />
+            </div>
+        </Router>
+    );
 }
 
-export default App;
+export default AppRouter;
---
+++ src/components/Recipe.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Recipe = (props) => {
+    return <h1>{props.recipe}</h1>
+};
+
+export default Recipe;
