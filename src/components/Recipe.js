186b9b5fcae725140275e693b93580e6c471f151
akirahin <niharikakulkarni@hotmail.com>
2019-08-23 23:18:22 +0530

    Add recipe details

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Ingredient from './Ingredient';
+
 const Recipe = (props) => {
-    return <h1>{props.recipe.name}</h1>
+    const {name, image, instructions, ingredients} = props.recipe;
+    return <div>
+        <h2>{name}</h2>
+        <img src={image} width={200} alt={name}/>
+        <Ingredient list={ingredients}/>
+        <div>{instructions}</div>
+    </div>
 };
 
 export default Recipe;
