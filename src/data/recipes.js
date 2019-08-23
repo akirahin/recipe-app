bd5cb17e5d2d1b975c4153f5e236eb8a7b12bde4
akirahin <niharikakulkarni@hotmail.com>
2019-08-23 23:02:11 +0530

    add images

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -8,6 +8,7 @@ const recipes = [
             "8 cups water",
             "1 box spaghetti"
         ],
+        image: 'https://images.unsplash.com/photo-1554295550-0b7069dfa034?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80'
     },
     {
         id: 2,
@@ -17,6 +18,7 @@ const recipes = [
             "2 Scoops Ice cream",
             "8 ounces milk"
         ],
+        image: 'https://images.unsplash.com/photo-1504712168631-d59c8516c409?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80'
     },
     {
         id: 3,
@@ -29,6 +31,7 @@ const recipes = [
             "1 pinch of salt",
             "pepper"
         ],
+        image: 'https://images.unsplash.com/photo-1541519227354-08fa5d50c44d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
     }
 ];
 
