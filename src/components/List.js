52d4837269d0b20f3d62e7c4b37590455ca9d18b
akirahin <niharikakulkarni@hotmail.com>
2019-08-24 16:16:45 +0530

    List Styling

--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styles from './List.module.scss'
 
 class List extends React.Component {
+    toggleHandler = (e) => {
+        const item = e.target;
+        item.classList.toggle(styles.bought)
+    };
+
     render() {
         const {ingredients} = this.props;
         return (
-            <div>
+            <div className={styles.container}>
                 <h2>Shopping List</h2>
-                <ul>
-                    {ingredients.map((item, id) => <li key={id}>{item}</li>)}
-                </ul>
+                <div className={styles.subHeader}>
+                    {ingredients.length ? 'Click on items to strike them off your list!' :'You dont need to buy anything!'}
+                </div>
+                <ol className={styles.shopList}>
+                    {ingredients.map((item, id) => <li key={id} onClick={this.toggleHandler}>{item}</li>)}
+                </ol>
             </div>
         )
     }
